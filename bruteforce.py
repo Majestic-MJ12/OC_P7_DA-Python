d8dbc22d26f7c4a1510e3e8a7b202b5f654cc040
Majestic-MJ12 <58705736+Majestic-MJ12@users.noreply.github.com>
2023-06-16 13:23:09 +0200

    Created the csv file needed

--- bruteforce.py
+++ bruteforce.py
@@ -7,4 +7,4 @@ def main():
     print(f"\nProcess {len(share_listing)} shares for {maximum}€ :")
 
     best_combination = set_combination(share_listing)
-    get_results(best_combinaison)
+    get_results(best_combination)
