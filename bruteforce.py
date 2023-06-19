5a8afa7ed1d3f78b1d9a3bbd2a6847c80bbfb597
Majestic-MJ12 <58705736+Majestic-MJ12@users.noreply.github.com>
2023-06-19 22:06:39 +0200

    The bruteforce is working. Also done the "UI"

--- bruteforce.py
+++ bruteforce.py
@@ -1,11 +1,10 @@
-import sys
-import csv
 import time
 import sys
+import pandas as pd
 from tqdm import tqdm
 from itertools import combinations
 
-start_time = time.time()
+begin_time = time.time()
 
 try:
     maximum = float(sys.argv[1])
@@ -14,33 +13,33 @@ except IndexError:
 
 
 def main():
-    share_listing = csv()
+    share_listing = read_csv()
 
-    print(f"\nProcess {len(share_listing)} shares for {maximum}€ :")
+    print(f"\nProcessing {len(share_listing)} shares for {maximum} euros :")
 
     best_combination = set_combination(share_listing)
-    get_results(best_combination)
+    display(best_combination)
 
 
-def csv():
-    with open("data/shares.csv") as csvfile:
-        shares_file = csv.reader(csvfile, delimiter=',')
+def read_csv():
+    df = pd.read_csv("csv/shares.csv", names=['Shares', 'Prices', 'Return'])
 
-        csv_list = []
-        for row in shares_file:
-            csv_list.append(
-                (row[0], float(row[1]), float(row[2]))
-            )
+    csv_list = []
+    for index, row in df.iterrows():
+        csv_list.append(
+            (row['Shares'], float(row['Prices']), float(row['Return']))
+        )
 
-        return csv_list
+    return csv_list
 
 
 def set_combination(csv_list):
     income = 0
     best_combination = []
 
-    for f in tqdm(range(len(csv_list))):
-        one_combination = all_combinations(csv_list, f + 1)
+    for f in tqdm(range(len(csv_list)), desc="\033[34mFinding the best combination among all\033[0m",
+                  bar_format="{l_bar}{bar:10}{r_bar}{bar:-10b}", ncols=100, colour='green'):
+        one_combination = combinations(csv_list, f + 1)
 
         for combination in one_combination:
             total_investment = calc_investment(combination)
@@ -53,3 +52,34 @@ def set_combination(csv_list):
                     best_combination = combination
 
     return best_combination
+
+
+def calc_investment(combination):
+    prices = []
+    for el in combination:
+        prices.append(el[1])
+
+    return sum(prices)
+
+
+def calc_income(combination):
+    all_income = []
+    for el in combination:
+        all_income.append(el[1] * el[2] / 100)
+
+    return sum(all_income)
+
+
+def display(best_combination):
+    print(f"\n\033[34mMost profitable investment ({len(best_combination)} in shares) :\033[0m\n")
+
+    for item in best_combination:
+        print(f"{item[0]} | {item[1]} euros | +{item[2]} %")
+
+    print("\n\033[34mThe total cost : \033[0m", calc_investment(best_combination), "euros")
+    print("\033[34mProfit after 2 years : +\033[0m", calc_income(best_combination), "euros")
+    print("\n\033[34mTime elapsed : \033[0m", time.time() - begin_time, "s")
+
+
+if __name__ == "__main__":
+    main()
