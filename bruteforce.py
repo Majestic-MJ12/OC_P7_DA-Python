dcf75261d6a3bb1f053ef9abda969582d8e8ba89
Majestic-MJ12 <58705736+Majestic-MJ12@users.noreply.github.com>
2023-06-27 16:54:54 +0200

    Added comments

--- bruteforce.py
+++ bruteforce.py
@@ -1,31 +1,41 @@
+# Import necessary libraries
 import time
 import sys
 import pandas as pd
 from tqdm import tqdm
 from itertools import combinations
 
+# Store the start time of the program
 begin_time = time.time()
 
+# Set the floating point format for pandas dataframes
 pd.set_option('float_format', '{:.2f}'.format)
 
+# Try to read the maximum investment amount from the command line arguments, and if it fails, set it to 500
 try:
     maximum = float(sys.argv[1])
 except IndexError:
     maximum = 500
 
 
+# The main function reads the CSV file, finds the best combination of shares, and displays the results.
 def main():
     share_listing = read_csv()
 
+    # Print a message indicating the number of shares being processed and the maximum investment amount
     print(f"\nProcessing {len(share_listing)} shares for {maximum} euros:")
 
+    # Find the best combination of shares and display the results
     best_combination = set_combination(share_listing)
     display(best_combination)
 
 
+# The read_csv function reads the CSV file and returns a list of tuples containing the share name, price, and return.
 def read_csv():
+    # Read the CSV file using pandas and assign column names
     df = pd.read_csv("csv/shares.csv", names=['Shares', 'Prices', 'Return'])
 
+    # Convert the dataframe to a list of tuples
     csv_list = []
     for index, row in df.iterrows():
         csv_list.append(
@@ -35,10 +45,13 @@ def read_csv():
     return csv_list
 
 
+# The set_combination function finds the best combination of shares that
+# maximizes the income while keeping the total investment below the maximum investment amount.
 def set_combination(csv_list):
     income = 0
     best_combination = []
 
+    # Use tqdm to display a progress bar while iterating over all possible combinations of shares.
     for f in tqdm(range(len(csv_list)), desc="\033[34mFinding the best combination among all\033[0m",
                   bar_format="{l_bar}{bar:10}{r_bar}{bar:-10b}", ncols=100, colour='green'):
         one_combination = combinations(csv_list, f + 1)
@@ -56,6 +69,7 @@ def set_combination(csv_list):
     return best_combination
 
 
+# The calc_investment function calculates the total investment required for a given combination of shares.
 def calc_investment(combination):
     prices = []
     for el in combination:
@@ -64,6 +78,7 @@ def calc_investment(combination):
     return sum(prices)
 
 
+# The calc_income function calculates the total income for a given combination of shares.
 def calc_income(combination):
     all_income = []
     for el in combination:
@@ -72,16 +87,23 @@ def calc_income(combination):
     return sum(all_income)
 
 
+# The display function displays the best combination of shares and the corresponding investment and income.
 def display(best_combination):
+    # Print a message indicating the number of shares in the best combination
     print(f"\n\033[34mMost profitable investment ({len(best_combination)} shares) :\033[0m\n")
 
+    # Print the details of each share in the best combination
     for item in best_combination:
         print(f"{item[0]} | {item[1]} euros | +{item[2]} %")
 
+    # Print the total cost and profit of the best combination
     print("\n\033[34mThe total cost : \033[0m", calc_investment(best_combination), "euros")
     print("\033[34mProfit after 2 years :\033[0m +", round(calc_income(best_combination), 2), "euros")
+
+    # Print the time elapsed since the start of the program
     print("\n\033[34mTime elapsed : \033[0m", round(time.time() - begin_time, 2), "s")
 
 
+# Call the main function if the script is run directly.
 if __name__ == "__main__":
     main()
--- optimized.py
+++ optimized.py
@@ -1,43 +1,56 @@
+# Import necessary libraries
 import csv
 import time
 import sys
 from tqdm import tqdm
 
+# Record the start time of the program
 begin_time = time.time()
 
+# Try to get the maximum investment amount from the command line arguments, default to 500 if not provided
 try:
     maximum = float(sys.argv[2])
 except IndexError:
     maximum = 500
 
 
+# Define the main function
 def main():
     try:
+        # Try to get the filename from the command line arguments
         file = "csv/" + sys.argv[1] + ".csv"
     except IndexError:
+        # If no filename is provided, print an error message and exit the program
         print("\n\x1b[34mNo file found. Try again.\x1b[0m\n")
         time.sleep(1)
         sys.exit()
 
+    # Read the CSV file and create a list of shares
     share_listing = read_csv(file)
 
+    # Print a message indicating the start of processing
     print(f"\n\x1b[34mProcessing '{sys.argv[1]}' ({len(share_listing)} shares) for {maximum} euros:\x1b[0m")
 
+    # Find the best combination of shares to invest in
     best_combination = kp(share_listing)
 
+    # Display the best combination of shares and the total cost and profit
     display(best_combination)
 
 
+# Define a function to read a CSV file and return a list of shares
 def read_csv(file):
     try:
         with open(file) as csvfile:
             shares_file = csv.reader(csvfile, delimiter=',')
 
+            # Skip the first row in both datasets
             if file != "csv/shares.csv":
-                next(csvfile)       # skip first row in both datasets
+                next(csvfile)
 
             share_listing = []
 
+            # Create a tuple for each share and add it to the list if it meets the criteria
             for i in shares_file:
                 if float(i[1]) <= 0 or float(i[2]) <= 0:
                     pass
@@ -52,23 +65,28 @@ def read_csv(file):
             return share_listing
 
     except FileNotFoundError:
+        # If the file is not found, print an error message and exit the program
         print(f"\n\x1b[34mFile '{file}' does not exist. Please try again.\x1b[0m\n")
         time.sleep(1)
         sys.exit()
 
 
+# Define a function to find the best combination of shares to invest in
 def kp(share_listing):
     max_investment = int(maximum * 100)
     share_total = len(share_listing)
     investment = []
     income = []
 
+    # Create lists of investments and incomes for each share
     for cut in share_listing:
         investment.append(cut[1])
         income.append(cut[2])
 
+    # Create a 2D list to store the optimal profit for each combination of shares and investment amount
     kps = [[0 for x in range(max_investment + 1)] for x in range(share_total + 1)]
 
+    # Use tqdm to display a progress bar while processing
     with tqdm(total=share_total, bar_format="\x1b[32m{l_bar}{bar:30}{r_bar}\x1b[0m") as pbar:
         for i in range(1, share_total + 1):
             pbar.update(1)
@@ -95,21 +113,25 @@ def kp(share_listing):
     return best_combination
 
 
+# Define a function to display the best combination of shares and the total cost and profit
 def display(best_combination):
     print(f"\n\x1b[34mThe most profitable investment ({len(best_combination)} shares):\x1b[0m\n")
 
     investment = []
     income = []
 
+    # Print information for each share in the best combination
     for item in best_combination:
         print(f"\x1b[34m{item[0]} | {round(item[1] / 100, 2)} euros | +{round(item[2], 2)} euros\x1b[0m")
         investment.append(round(item[1] / 100, 2))
         income.append(round(item[2], 2))
 
+    # Print the total cost and profit
     print("\n\x1b[34mThe total cost : ", round(sum(investment), 2), "euros")
     print("Profit after 2 years : +", round(sum(income), 2), "euros")
     print("\nTime elapsed : ", round(time.time() - begin_time, 2), "s\n")
 
 
+# Call the main function if this file is being run as the main program
 if __name__ == "__main__":
     main()
